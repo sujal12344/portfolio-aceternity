f1b27c5d239bbc162e35a8a53d9fc2c27b445224
Sujal <sujalkesharwani220@gmail.com>
2024-08-24 18:30:46 +0530

    Projects added in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Grid from "@/components/Grid";
 // import Clients from "@/components/Clients";
 // import Approach from "@/components/Approach";
 // import Experience from "@/components/Experience";
-// import RecentProjects from "@/components/RecentProjects";
+import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
@@ -18,7 +18,7 @@ const Home = () => {
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
-        {/* <RecentProjects /> */}
+        <RecentProjects />
         {/* <Clients /> */}
         {/* <Experience /> */}
         {/* <Approach /> */}
