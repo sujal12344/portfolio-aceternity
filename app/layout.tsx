833ed7d211b9a23e205fbf066328238e09d053a5
Sujal <sujalkesharwani220@gmail.com>
2024-09-04 01:39:54 +0530

    favicon updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   title: "Sujal's Portfolio",
   description: "Modern Portfolio",
   icons:
-    "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/52c99e53-660d-4306-80aa-338eaccccc1d/dgly8fd-6a0884bb-a0ac-483d-a784-1d9b282a036c.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzUyYzk5ZTUzLTY2MGQtNDMwNi04MGFhLTMzOGVhY2NjY2MxZFwvZGdseThmZC02YTA4ODRiYi1hMGFjLTQ4M2QtYTc4NC0xZDliMjgyYTAzNmMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.yf7O0nxapc_cqufyLERML-s1JffsvyCeFsEil5NZSm0",
+    "https://i.pinimg.com/originals/a0/e2/9e/a0e29eb2aec8fd669977ea1c5220ccd3.jpg",
 };
 
 export default function RootLayout({
