a1706d9f43d1fd808ab245dec91704f5b4166f0a
Sujal <sujalkesharwani220@gmail.com>
2024-09-04 19:39:31 +0530

    update according to mobile

--- data/index.ts
+++ data/index.ts
@@ -29,7 +29,7 @@ export const gridItems = [
   },
   {
     id: 3,
-    title: "My tech stack",
+    title: "My Tech Stack",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
