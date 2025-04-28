2a61d0e61d9ea9b26d87b6cf30b12aa4be30abbc
sujal <sujalkesharwani220@gmail.com>
2025-04-28 11:48:04 +0530

    fix all issues

--- components/ui/BenoteGrid.tsx
+++ components/ui/BenoteGrid.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
 
-import Lottie from "react-lottie";
+// import Lottie from "lottie-react";
 
 import { cn } from "@/lib/utils";
 
